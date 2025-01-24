0800309287b1e321857f6c53300020458eb5c6fe
biarog <66644264+biarog@users.noreply.github.com>
2025-01-23 22:53:12 -0300

    Feitos modelos iniciais

--- scripts/experimentos.py
+++ scripts/experimentos.py
@@ -12,3 +12,20 @@
 # ################################################################
 
 # Arquivo com todas as funcoes e codigos referentes aos experimentos
+
+# Retorna a matriz de confusão
+def matriz_confusao(Y_test, Y_pred):
+    from sklearn.metrics import confusion_matrix
+    
+    return confusion_matrix(Y_test, Y_pred)
+
+# Retorna precisao, revocação, fscore separado por classe
+def metricas(Y_test, Y_pred, media):
+    from sklearn.metrics import precision_recall_fscore_support
+    
+    return precision_recall_fscore_support(Y_test, Y_pred, average=media)
+
+def acuracia(Y_test, Y_pred):
+    from sklearn.metrics import accuracy_score
+    
+    return accuracy_score(Y_test, Y_pred)
